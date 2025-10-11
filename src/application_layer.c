e6ce21aba52a4a8c8591e14d360d1dbc978f8ae0
Rui <up202307738@up.pt>
2025-10-11 18:43:23 +0100

    Application layer changes

--- src/application_layer.c
+++ src/application_layer.c
@@ -7,5 +7,24 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+    LinkLayer linkLayer;
+    strcpy(linkLayer.serialPort, serialPort);
+    linkLayer.baudRate = baudRate;
+    linkLayer.nRetransmissions = nTries;
+    linkLayer.timeout = timeout;
+    //linkLayer.role = (role[0]== "LlTx") ? LlTx : LlRx
+
+    llopen(linkLayer);
+
+    if (linkLayer.role == LlTx)
+    {
+        //Transmitter
+        llwrite(NULL);
+    }
+    else
+    {
+        //Receiver
+        llread(NULL);
+    }
+
 }
--- src/link_layer.c
+++ src/link_layer.c
@@ -6,14 +6,29 @@
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
+#define FLAG 0x7E
+#define A_SE 0x03
+#define A_RE 0x01
+#define C_SET 0x03
+#define C_UA 0x07
+#define C_RR0 0xAA
+#define C_RR1 0xAB
+#define C_REJ0 0x54
+#define C_REJ1 0x55
+#define C_DISC 0x0B
+#define BCC(a,b) = a^b
+#define CI0 0x00
+#define CI1 0x80
+
+
+
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    // TODO: Implement this function
+    return openSerialPort(connectionParameters.serial_port, connectionParameters.baudRate);
 
-    return 0;
 }
 
 ////////////////////////////////////////////////
@@ -21,9 +36,11 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    // TODO: Implement this function
-
-    return 0;
+    for (int i=0; i< bufSize; i++){
+        writeBytesSerialPort(buf, bufSize);
+    }
+    
+    return bufSize;
 }
 
 ////////////////////////////////////////////////
